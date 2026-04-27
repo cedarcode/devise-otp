618a04cf072846a294ec3d60ed1e89394b6ac69f
Santiago Rodriguez <46354312+santiagorodriguez96@users.noreply.github.com>
2026-04-27 12:06:31 -0300

    test: update dummy app

--- test/dummy/app/models/admin.rb
+++ test/dummy/app/models/admin.rb
@@ -1,6 +1,6 @@
 class Admin < ActiveRecord::Base
-  devise :otp_authenticatable, :database_authenticatable, :registerable,
-    :trackable, :validatable
+  devise :database_authenticatable, :registerable, :trackable, :validatable,
+    :two_factor_authenticatable, two_factor_methods: [:otp]
 
   # Setup accessible (or protected) attributes for your model
   # attr_accessible :otp_enabled, :otp_mandatory, :as => :otp_privileged
